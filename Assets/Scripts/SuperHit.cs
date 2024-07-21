3f88929574b46ca54e93fa4ff874d8d6ccfae0b7
NikoDav <165057786+NikoDav@users.noreply.github.com>
2024-07-21 12:35:39 -0400

    Added super hit jump

--- Assets/Scripts/SuperHit.cs
+++ Assets/Scripts/SuperHit.cs
@@ -9,31 +9,28 @@ public class SuperHit : MonoBehaviour
     [SerializeField] private PlayerController _player;
     [SerializeField] private Rigidbody2D _rigidbody2D;
     [SerializeField] private Transform _hand;
-
+    [SerializeField] private Animator _animator;
 
 
     public void SuperHitSetup()
     {
-        //_player.transform.position = new Vector2(transform.position.x, 0.6f);
-        //_gameManager.CurrentBall.transform.position = new Vector2(_player.transform.position.x - 0.5f, _player.transform.position.y);
-        _rigidbody2D.AddForce(new Vector2(0, 100), ForceMode2D.Impulse);
-        Debug.Log(1234568);
-      //  StartCoroutine(Throw());
+        _player.GameManager.CurrentBall.transform.parent = null;
+        _player.GameManager.CurrentBall.GetComponent<Rigidbody2D>().AddForce(new Vector2(2, -1)) ;
     }
 
     private IEnumerator Throw()
     {
         yield return new WaitForSeconds(0.1f);
-        _player.GameManager.CurrentBall.transform.parent = null;
-        _player.GameManager.CurrentBall.GetComponent<Rigidbody2D>().AddForce(new Vector2()) ;
+        
     }
 
     public void SetBall()
     {
         _player.GameManager.CurrentBall.transform.parent = _hand;
-        _player.GameManager.CurrentBall.transform.position = _hand.position;
+       // _player.GameManager.CurrentBall.transform.position = _hand.position;
         _player.GameManager.CurrentBall.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         _player.ChangeState(State.SuperHit);
-        SuperHitSetup();
+        _animator.SetTrigger("SuperHit");
+     //   SuperHitSetup();
     }
 }
